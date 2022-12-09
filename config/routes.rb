daa3a3cb18ac1c7450c33da5de7f24089a5604e8
Will <wneal_19@yahoo.co.uk>
2022-12-09 13:04:48 +0000

    mounted presign end point

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'books_uploads#new'
   devise_for :users
   resources :books_uploads
+  mount BooksUploader.presign_endpoint(:cache) => "/s3/params"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
