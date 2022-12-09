22ef3f1a64929a365ea81a90cbeada160faa9aab
Will <wneal_19@yahoo.co.uk>
2022-12-09 13:00:10 +0000

    initialize uppy stuff

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+
+
+// if you are using turbo, listen on turbo:load instead
+// listen on 'turbolinks:load' instead of 'DOMContentLoaded' if using Turbolinks
+document.addEventListener('turbo:load', () => {
+  document.querySelectorAll('.upload-file').forEach(fileInput => {
+    fileUpload(fileInput)
+  })
+})
